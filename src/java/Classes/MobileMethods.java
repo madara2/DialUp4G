fc96008ddd5df03e9c33be94d79b58075e55f8b9
dewminh2 <rathnayake.gunawardhana@students.plymouth.ac.uk>
2015-03-23 11:55:56 -0700

    mobile methods

---
+++ src/java/Classes/MobileMethods.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import static Classes.UserMethods.conn;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Derwmin Hasitha
+ */
+public class MobileMethods {
+     static Connection conn;
+    static Statement stmt;
+    static String query;
+    static ResultSet rs;
+}
+public static int saveMobile(AddNewMobile){}
