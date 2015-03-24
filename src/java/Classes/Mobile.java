c5d35716579e0e891fec84b7789237d872ce7b6d
dewminh2 <rathnayake.gunawardhana@students.plymouth.ac.uk>
2015-03-24 15:36:40 +0530

    admin methods/ admin user view

---
+++ src/java/Classes/Mobile.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Dewmin
+ */
+public class Mobile {
+
+    static Connection conn;
+    static Statement stmt;
+    static String query;
+    static ResultSet rs;
+    
+    public static int saveMobile(){
+        return 0;
+    }
+}
